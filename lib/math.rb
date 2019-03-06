44223f321f9946208dd626721d5c0d9ba37f62fd
Aman Singhla <singhlaaman@gmail.com>
2019-03-06 21:10:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,12 +15,14 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  num1 + ((num2))
 end
